5c3d5678700bb11f1a0f3c3c43d216bf6fb25483
mahmut.akyol@outlook.com.tr <mahmut.akyol@outlook.com.tr>
2016-11-27 00:16:36 +0200

    Initial Commit

---
+++ StarAlgorithmWithHMMAndPHHM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StarAlgorithmWithHMMAndPHHM
+//
+//  Created by mahmut akyol on 27.11.2016.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
