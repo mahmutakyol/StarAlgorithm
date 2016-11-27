dc6341f833c773f517202fc125603f282f426ca1
mahmut.akyol@outlook.com.tr <mahmut.akyol@outlook.com.tr>
2016-11-27 17:27:54 +0200

    io test

--- StarAlgorithmWithHMMAndPHHM/main.cpp
+++ StarAlgorithmWithHMMAndPHHM/main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 
+// c++ read and write library
+#include <fstream>
 using namespace std;
+
+
 
 int main(int argc, const char * argv[]) {
     
+    char data[100];
     
-    cout << "test";
+    // open a file in write mode.
+    ofstream outfile;
+    outfile.open("input.txt");
+    
+    cout << "Writing to the file" << endl;
+    cout << "Enter your name: ";
+    cin.getline(data, 100);
+    
+    // write inputted data into the file.
+    outfile << data << endl;
+    
+    cout << "Enter your age: ";
+    cin >> data;
+    cin.ignore();
+    
+    // again write inputted data into the file.
+    outfile << data << endl;
+    
+    // close the opened file.
+    outfile.close();
+    
+    // open a file in read mode.
+    ifstream infile;
+    infile.open("input.txt");
+    
+    cout << "Reading from the file" << endl;
+    infile >> data;
+    
+    // write the data at the screen.
+    cout << data << endl;
+    
+    // again read the data from the file and display it.
+    infile >> data;
+    cout << data << endl;
+    
+    // close the opened file.
+    infile.close();
     
     return 0;
     
